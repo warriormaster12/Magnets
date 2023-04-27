c4c11b579467dc6ff26d412017a90f9c123254a1
Veikko-P <vpentika@gmail.com>
2023-04-27 19:46:51 +0300

    Player Edits

--- Scripts/interactibleScript.gd
+++ Scripts/interactibleScript.gd
@@ -1,9 +1,7 @@
-extends Sprite2D
+extends Node2D
 
 class_name Interactible
 
 enum POLARITIES{Negative=0, Positive=1, Neutral=2}
-
-@export var interactible_name: String
 
 @export var polarity: POLARITIES
--- Scripts/playerScript.gd
+++ Scripts/playerScript.gd
@@ -13,6 +13,10 @@ enum POLARITIES{Negative=0, Positive=1}
 @export var acceleration = 1000
 @export var deceleration = 2000
 
+# Turning speed variables
+@export var max_turn_speed = 10.0
+@export var min_turn_speed = 2.0
+
 func _process(delta):
 	# Check for player movement input
 	var input_vector = Vector2.ZERO
@@ -45,4 +49,33 @@ func _process(delta):
 	var node_position = position
 	node_position += velocity * delta
 	position = node_position
+	
+	# Get the direction to the mouse
+	var mouse_pos = get_global_mouse_position()
+	var direction = mouse_pos - global_position
+	direction = direction.normalized()
+
+	# Calculate the angle to the mouse
+	var angle = direction.angle()
+
+	# Get the current rotation of the player
+	var current_rotation = rotation
+
+	# Calculate the shortest angle between the player's current rotation and the target angle
+	var shortest_angle = wrapf(angle - current_rotation, -TAU / 2.0, TAU / 2.0)
+
+	# Calculate the turn speed based on the distance from the target angle
+	var distance = abs(shortest_angle)
+	var turn_speed_ratio = 1.0 - clamp(distance / (TAU / 2.0), 0.0, 1.0)
+	var turn_speed = lerp(min_turn_speed, max_turn_speed, turn_speed_ratio)
+
+	# Rotate the player towards the mouse
+	var max_rotation = turn_speed * delta
+	var new_rotation = current_rotation + sign(shortest_angle) * min(abs(shortest_angle), max_rotation)
+	rotation = new_rotation
+
+	# Rotate the child Sprite2D node
+	self.rotation = new_rotation
+	
+	# Creates a detection area infront of the Sprite2D in the direction its facing
 	
