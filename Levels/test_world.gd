9482266fc9eb9ba8b50f8a23648169fa9c0f0269
warriormaster12 <streng.alexander@outlook.com>
2023-04-28 20:56:01 +0300

    multiplayer should work across the internet

--- Levels/test_world.gd
+++ Levels/test_world.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 
 @onready var host_join:Control = $CanvasLayer/HostJoin
@@ -6,6 +6,7 @@ extends Node2D
 
 const PLAYER = preload("res://GameObjects/player_1.tscn")
 const PORT:int = 9999
+var address:String = ""
 var enet_peer := ENetMultiplayerPeer.new()
 
 
@@ -29,14 +30,20 @@ func _on_host_pressed():
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
 	
 	add_player(multiplayer.get_unique_id())
+	
+	upnp_setup()
 
 
 func _on_join_pressed():
 	host_join.hide()
 	
-	enet_peer.create_client("localhost", PORT)
+	if not address.is_empty():
+		var err:Error = enet_peer.create_client(address, PORT)
+		if err != OK:
+			print("can't connect to this ip address ",err)
 	multiplayer.multiplayer_peer = enet_peer
 	
 
@@ -45,3 +52,26 @@ func add_player(peer_id:int)->void:
 	var player = PLAYER.instantiate()
 	player.name = str(peer_id)
 	add_child(player)
+func remove_player(peer_id:int)->void:
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+
+
+func _on_address_text_changed(new_text: String) -> void:
+	address = new_text
+
+func upnp_setup():
+	var upnp := UPNP.new()
+	var discover_result:int = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Discover Failed! Error %s" % discover_result)
+	
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		"UPNP Failed To Discover Valid Gateway")
+	
+	var map_result:int = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address())
