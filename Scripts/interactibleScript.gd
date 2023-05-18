f9c051bbc551566324936147a6b85a4c680f6b16
Veikko-P <vpentika@gmail.com>
2023-05-18 18:51:10 +0300

    Boxes & Magnets

--- Scripts/interactibleScript.gd
+++ Scripts/interactibleScript.gd
@@ -5,3 +5,9 @@ class_name Interactible
 enum POLARITIES{Negative=0, Positive=1, Neutral=2}
 
 @export var polarity: POLARITIES
+
+func _ready():
+	if (polarity == POLARITIES.Negative):
+		add_to_group("NegativePolarity")
+	elif (polarity == POLARITIES.Positive):
+		add_to_group("PositivePolarity")
--- Scripts/magnetismRay.gd
+++ Scripts/magnetismRay.gd
@@ -10,17 +10,13 @@ enum POLARITIES{Negative=0, Positive=1}
 @export var rayStrength: float
 
 func _ready():
-	self.target_position.x = rayStrength
+	self.target_position.x = rayLength
 
 func _physics_process(_delta):
-	var colliders = get_collider()
-	if colliders:
-		for collider in colliders:
-			if (collider.has_method(POLARITIES)):
-				var direction = (self.global_transform - collider.global_transform).normalized()
-				var distance = (self.global_transform - collider.global_transform).length()
-				var targetPolarity: POLARITIES = collider.POLARITIES
-				if (polarity == targetPolarity):
-					collider.apply_central_impulse(direction * (1 / distance))
-				else:
-					collider.apply_central_impulse(-direction * (1 / distance))
+	var collider = get_collider()
+	if collider:
+		var direction = (self.global_position - collider.global_position).normalized()
+		if (collider.is_in_group("NegativePolarity")):
+			collider.apply_central_impulse(-direction * rayStrength)
+		elif (collider.is_in_group("PositivePolarity")):
+			collider.apply_central_impulse(direction * rayStrength)
