d084f22d28033b3018f991e4b9214b3d5d8ec319
Veikko-P <vpentika@gmail.com>
2023-05-18 18:24:45 +0300

    Magnet Ray Fixed

--- Scripts/magnetismRay.gd
+++ Scripts/magnetismRay.gd
@@ -6,23 +6,21 @@ enum POLARITIES{Negative=0, Positive=1}
 
 @export var polarity: POLARITIES
 @export var extendUntilWall: bool
-@export var rayWidth: float
 @export var rayLength: float
 @export var rayStrength: float
 
 func _ready():
-	$RayCast2D.length = rayLength
-	$RayCast2D.width = rayWidth
-	$RayCast2D.force = rayStrength
+	self.target_position.x = rayStrength
 
 func _physics_process(_delta):
 	var colliders = get_collider()
-	for collider in colliders:
-		if (collider.has_method(POLARITIES)):
-			var direction = ($RayCast2D.global_transform - collider.global_transform).normalized()
-			var distance = ($RayCast2D.global_transform - collider.global_transform).length()
-			var targetPolarity: POLARITIES = collider.POLARITIES
-			if (polarity == targetPolarity):
-				collider.apply_central_impulse(direction * (1 / distance))
-			else:
-				collider.apply_central_impulse(-direction * (1 / distance))
+	if colliders:
+		for collider in colliders:
+			if (collider.has_method(POLARITIES)):
+				var direction = (self.global_transform - collider.global_transform).normalized()
+				var distance = (self.global_transform - collider.global_transform).length()
+				var targetPolarity: POLARITIES = collider.POLARITIES
+				if (polarity == targetPolarity):
+					collider.apply_central_impulse(direction * (1 / distance))
+				else:
+					collider.apply_central_impulse(-direction * (1 / distance))
--- Scripts/plateScript.gd
+++ Scripts/plateScript.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-class_name PressurePlate
 
 var objectsToDetect = []
 
